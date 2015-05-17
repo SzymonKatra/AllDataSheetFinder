76ac4d19d987ad8277d8fb727b156a466d4c0202
Szymon Katra <szymon.katra@gmail.com>
2015-05-17 19:06:43 +0200

    Added exception handling in updater application

--- AllDataSheetFinderUpdater/Program.cs
+++ AllDataSheetFinderUpdater/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AllDataSheetFinderUpdater
 {
@@ -39,19 +40,41 @@ namespace AllDataSheetFinderUpdater
             if (!Directory.Exists(destinationPath)) Directory.CreateDirectory(destinationPath);
             foreach (string file in Directory.EnumerateFiles(destinationPath, "*.*", SearchOption.AllDirectories))
             {
-                File.Delete(file);
+                try
+                {
+                    File.Delete(file);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem while deleting file: " + Environment.NewLine + file + Environment.NewLine + e.Message);
+                }
             }
-            foreach (string file in Directory.EnumerateDirectories(destinationPath, "*", SearchOption.AllDirectories))
+            foreach (string directory in Directory.EnumerateDirectories(destinationPath, "*", SearchOption.AllDirectories))
             {
-                Directory.Delete(file, true);
+                try
+                {
+                    Directory.Delete(directory, true);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem while deleting directory: " + Environment.NewLine + directory + Environment.NewLine + e.Message);
+                }
             }
 
             foreach (string file in Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories))
             {
                 string relativeFilePath = file.Substring(sourcePath.Length + 1);
                 string resultFilePath = destinationPath + Path.DirectorySeparatorChar + relativeFilePath;
-                if (!Directory.Exists(Path.GetDirectoryName(resultFilePath))) Directory.CreateDirectory(Path.GetDirectoryName(resultFilePath));
-                File.Copy(file, resultFilePath);
+
+                try
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(resultFilePath))) Directory.CreateDirectory(Path.GetDirectoryName(resultFilePath));
+                    File.Copy(file, resultFilePath);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Problem while copying file from: " + Environment.NewLine + file + " to: " + Environment.NewLine + resultFilePath + Environment.NewLine + e.Message);
+                }
             }
 
             Process.Start(destinationPath + Path.DirectorySeparatorChar + "AllDataSheetFinder.exe");
