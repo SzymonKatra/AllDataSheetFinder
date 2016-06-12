8df857fa511e896d62018fadf97f692248eb796c
Szymon Katra <szymon.katra@gmail.com>
2016-06-12 17:08:15 +0200

    String interpolation in updater

--- AllDataSheetFinderUpdater/Program.cs
+++ AllDataSheetFinderUpdater/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AllDataSheetFinderUpdater
@@ -51,7 +47,7 @@ namespace AllDataSheetFinderUpdater
                 }
                 catch (Exception e)
                 {
-                    MessageBox.Show("Problem while deleting file: " + Environment.NewLine + file + Environment.NewLine + e.Message);
+                    MessageBox.Show($"Problem while deleting file:{Environment.NewLine}{file}{Environment.NewLine}{e.Message}");
                 }
             }
             foreach (string directory in Directory.EnumerateDirectories(destinationPath, "*", SearchOption.AllDirectories))
@@ -62,14 +58,14 @@ namespace AllDataSheetFinderUpdater
                 }
                 catch (Exception e)
                 {
-                    MessageBox.Show("Problem while deleting directory: " + Environment.NewLine + directory + Environment.NewLine + e.Message);
+                    MessageBox.Show($"Problem while deleting directory:{Environment.NewLine}{directory}{Environment.NewLine}{e.Message}");
                 }
             }
 
             foreach (string file in Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories))
             {
                 string relativeFilePath = file.Substring(sourcePath.Length + 1);
-                string resultFilePath = destinationPath + Path.DirectorySeparatorChar + relativeFilePath;
+                string resultFilePath = $"{destinationPath}{Path.DirectorySeparatorChar}{relativeFilePath}";
 
                 try
                 {
@@ -78,11 +74,11 @@ namespace AllDataSheetFinderUpdater
                 }
                 catch (Exception e)
                 {
-                    MessageBox.Show("Problem while copying file from: " + Environment.NewLine + file + " to: " + Environment.NewLine + resultFilePath + Environment.NewLine + e.Message);
+                    MessageBox.Show($"Problem while copying file from:{Environment.NewLine}{file} to: {Environment.NewLine}{resultFilePath}{Environment.NewLine }{e.Message}");
                 }
             }
 
-            Process.Start(destinationPath + Path.DirectorySeparatorChar + "AllDataSheetFinder.exe");
+            Process.Start($"{destinationPath}{Path.DirectorySeparatorChar}AllDataSheetFinder.exe");
         }
     }
 }
